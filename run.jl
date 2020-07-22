01b8de36d9bdb9c7d6b03af485bc3c87856f6adf
Sascha Mann <git@mail.saschamann.eu>
2020-07-22 22:05:48 +0200

    Strip Julia build paths

--- run.jl
+++ run.jl
@@ -7,6 +7,12 @@ const OUTPUT_DIR = ARGS[3]
 @debug "OUTPUT_DIR = " OUTPUT_DIR
 
 let json_str = tojson(runtests(joinpath(SOLUTION_DIR, "runtests.jl"))...)
+    # Strip dir inside the test-runner container
     cleaned_json_str = replace(json_str, SOLUTION_DIR => "./")
+
+    # Strip Julia build env directories
+    # This may need to be changed if the build environment of Julia changes in future versions
+    cleaned_json_str = replace(cleaned_json_str, "/buildworker/worker/package_linux64/build/usr/share" => "[...]")
+
     write(joinpath(OUTPUT_DIR, "results.json"), cleaned_json_str)
 end
