6bec4e54efb67d9368f0cc9f49f5d894a632b21b
Colin Caine <cmcaine@gmail.com>
2021-08-30 17:29:02 +0100

    Make huge diff slightly smaller.

--- precompile_execution_file.jl
+++ precompile_execution_file.jl
@@ -1,6 +1,6 @@
 # This file is required for PackageCompiler
 # See https://julialang.github.io/PackageCompiler.jl/dev/sysimages/#tracing-1 for more info
 
-using ExercismTestReports: test_runner
+using ExercismTestReports
 
 mktempdir(dir -> test_runner("", "test/fixtures/everything_at_once", dir))
