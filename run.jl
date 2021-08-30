955e99966486cb7d1e9025d9516080c133644552
Colin Caine <cmcaine@gmail.com>
2021-08-30 12:04:17 +0100

    Make testable

--- run.jl
+++ run.jl
@@ -1,18 +1,3 @@
 using ExercismTestReports
 
-const SOLUTION_DIR = ARGS[2]
-const OUTPUT_DIR = ARGS[3]
-
-@debug "SOLUTION_DIR = " SOLUTION_DIR
-@debug "OUTPUT_DIR = " OUTPUT_DIR
-
-let json_str = tojson(runtests(joinpath(SOLUTION_DIR, "runtests.jl"))...)
-    # Strip dir inside the test-runner container
-    cleaned_json_str = replace(json_str, SOLUTION_DIR => "./")
-
-    # Strip Julia build env directories
-    # This may need to be changed if the build environment of Julia changes in future versions
-    cleaned_json_str = replace(cleaned_json_str, "/buildworker/worker/package_linux64/build/usr/share" => "[...]")
-
-    write(joinpath(OUTPUT_DIR, "results.json"), cleaned_json_str)
-end
+test_runner(ARGS[1], ARGS[2], ARGS[3])
--- src/ExercismTestReports.jl
+++ src/ExercismTestReports.jl
@@ -5,6 +5,38 @@ include("runner.jl")
 include("tojson.jl")
 
 export ReportingTestSet
-export runtests, tojson
+export test_runner
+
+"""
+    test_runner(exercise_slug, solution_dir)
+
+Like the three-argument version but returns a String encoding the contents of results.json instead of writing it to a file.
+
+This version is used in the tests for the test_runner itself.
+"""
+function test_runner(exercise_slug, solution_dir)
+    captured_stdout, testsets = runtests(joinpath(solution_dir, "runtests.jl"))
+    json_str = tojson(captured_stdout, testsets)
+
+    # Strip dir inside the test-runner container
+    cleaned_json_str = replace(json_str, solution_dir => "./")
+
+    # Strip Julia build env directories
+    # This may need to be changed if the build environment of Julia changes in future versions
+    cleaned_json_str = replace(cleaned_json_str, "/buildworker/worker/package_linux64/build/usr/share" => "[...]")
+
+    return cleaned_json_str
+end
+
+"""
+    test_runner(exercise_slug, solution_dir, output_dir)
+
+Implements the interface described below.
+
+https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+"""
+function test_runner(exercise_slug, solution_dir, output_dir)
+    write(joinpath(output_dir, "results.json"), test_runner(exercise_slug, solution_dir))
+end
 
 end
