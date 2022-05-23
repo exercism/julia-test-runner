5621c220130dd9a17602dc90e6a1c4c504a200e2
Colin Caine <cmcaine@gmail.com>
2022-05-23 13:41:40 +0100

    Simplify test_code conditions
    Co-authored-by: Sascha Mann <git@mail.saschamann.eu>

--- src/tojson.jl
+++ src/tojson.jl
@@ -85,13 +85,11 @@ function tojson(output::String, ts::ReportingTestSet)
     output = truncate_output(output)
 
     function test_code(result::Test.Result)
-        if result isa Test.Pass && result.test_type == :test_throws
-            "@test_throws $(result.data) $(result.orig_expr)"
-        elseif result isa Test.Fail && startswith(string(result.test_type), "test_throws")
+        if startswith(string(result.test_type), "test_throws")
             "@test_throws $(result.data) $(result.orig_expr)"
         elseif result isa Test.LogTestFailure
             "@test_logs $(join(result.patterns, ' ')) $(result.orig_expr)"
-        elseif result isa Test.Error && result.test_type == :test_unbroken
+        elseif result.test_type == :test_unbroken
             "@test_broken $(result.orig_expr)"
         elseif result isa Test.Broken
             macro_name = result.test_type === :skipped ? "@test_skip " : "@test_broken "
