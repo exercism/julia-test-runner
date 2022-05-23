07df7d2e180e8d2b5e121ed765702a7120730922
Colin Caine <cmcaine@gmail.com>
2022-05-23 04:32:53 +0100

    CI and dev tests now ignore all but first line of error messages

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using ExercismTestReports
 using Test
+using JSON
 import InteractiveUtils
 
 # When running tests on this project, the solution_dir path is likely to be
@@ -18,6 +19,13 @@ const TMP_FIXTURES = joinpath(TEMPDIR, "fixtures")
     ])
 end
 
+# Shorten the error messages so we don't need to care about specific paths, file numbers, etc.
+function shorten_messages!(result)
+    if result["status"] == "error" && haskey(result, "message")
+        result["message"] = split(result["message"], '\n')[1]
+    end
+end
+
 @testset for fixture in readdir(TMP_FIXTURES)
     results = test_runner("", "$TMP_FIXTURES/$fixture/")
     if endswith(fixture, "syntax_error")
@@ -26,6 +34,12 @@ end
         TEST_PREFIX = "~/projects/exercism/repos/julia-test-runner"
         results = replace(results, HOME_PREFIX => TEST_PREFIX)
     end
+
+    dict_results = JSON.parse(results)
+    shorten_messages!(dict_results) # For syntax errors
+    map(shorten_messages!, dict_results["tests"])
+    results = JSON.json(dict_results, 4)
+
     reference = "$FIXTURES/$fixture/results.json"
     if isfile(reference)
         ref = read(reference, String)
